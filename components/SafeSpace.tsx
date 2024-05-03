c42e1b4de920ca218271276568b212139b63cf1f
dtun <dtun@me.com>
2024-05-02 19:11:55 -0700

    Add SafeSpace

---
+++ components/SafeSpace.tsx
@@ -0,0 +1,18 @@
+import { View } from "react-native";
+import {
+  withSafeAreaInsets,
+  WithSafeAreaInsetsProps,
+} from "react-native-safe-area-context";
+
+function SafeBottom(props: WithSafeAreaInsetsProps) {
+  return <View style={{ height: props.insets.bottom }} />;
+}
+
+function SafeTop(props: WithSafeAreaInsetsProps) {
+  return <View style={{ height: props.insets.top }} />;
+}
+
+export const SafeSpace = {
+  Top: withSafeAreaInsets(SafeTop),
+  Bottom: withSafeAreaInsets(SafeBottom),
+};
