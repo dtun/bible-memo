3c1fcb8214a48cb617cebd5b8982ff95846f1356
dtun <dtun@me.com>
2024-05-04 07:34:40 -0700

    Update error script

--- scripts/eas-build-on-error.sh
+++ scripts/eas-build-on-error.sh
@@ -1,26 +1,9 @@
 #!/bin/bash
 
-# Variables
-TOKEN="${GITHUB_API_KEY}"
-BUILD_ID="${EAS_BUILD_ID}"
-BUILD_PLATFORM="${EAS_BUILD_PLATFORM}"
-OWNER='dtun'
-REPO='bible'
-TITLE="${BUILD_PLATFORM}: E2E Build Failure"
-BODY="The EAS build failed on platform ${BUILD_PLATFORM} with build ID ${BUILD_ID}."
-
-# GitHub API URL
-URL="https://api.github.com/repos/$OWNER/$REPO/issues"
-
-# JSON data
-DATA=$(jq -n --arg title "$TITLE" --arg body "$BODY" \
-    '{"title": $title, "body": $body}')
-
-# Sending request to GitHub API to create issue
 curl -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer $TOKEN" \
+  -H "Authorization: Bearer ${GITHUB_API_KEY}" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  "$URL" \
-  -d "$DATA"
+  https://api.github.com/repos/dtun/bible/issues \
+  -d "{"title":"${BUILD_PLATFORM}: E2E Build Failure","body":"The EAS build failed on platform ${BUILD_PLATFORM} with build ID ${BUILD_ID}.","assignees":["dtun"]"}"
