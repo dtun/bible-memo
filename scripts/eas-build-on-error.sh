74bc2cfc999df3c9e7aa1da449bd4b7a3ec40a88
dtun <dtun@me.com>
2024-05-04 07:14:52 -0700

    Update error script

--- scripts/eas-build-on-error.sh
+++ scripts/eas-build-on-error.sh
@@ -17,7 +17,10 @@ DATA=$(jq -n --arg title "$TITLE" --arg body "$BODY" \
     '{"title": $title, "body": $body}')
 
 # Sending request to GitHub API to create issue
-curl -s -H "Authorization: token $TOKEN" \
-     -H "Content-Type: application/json" \
-     -d "$DATA" \
-     "$URL"
+curl -L \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $TOKEN" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  "$URL" \
+  -d "$DATA"
