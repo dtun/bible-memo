3073bbd59b9e2d4b18733d87f8fbd1b2b81faa91
dtun <dtun@me.com>
2024-05-04 08:43:38 -0700

    Update error script

--- scripts/eas-build-on-error.sh
+++ scripts/eas-build-on-error.sh
@@ -6,4 +6,4 @@ curl -L \
   -H "Authorization: Bearer ${GITHUB_API_KEY}" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/dtun/bible/issues \
-  -d "{"title":"${BUILD_PLATFORM}: E2E Build Failure","body":"The EAS build failed on platform ${BUILD_PLATFORM} with build ID ${BUILD_ID}.","assignees":["dtun"]"}"
+  -d "{\"title\":\"${BUILD_PLATFORM}: E2E Build Failure\",\"body\":\"The EAS build failed on platform ${BUILD_PLATFORM} with build ID ${BUILD_ID}.\",\"assignees\":[\"dtun\"]}"
