55ee1c2614c29f0baf720d4b383cacd527f8dfa7
dtun <dtun@me.com>
2024-05-04 06:47:24 -0700

    Update error script

--- scripts/eas-build-on-error.sh
+++ scripts/eas-build-on-error.sh
@@ -6,15 +6,15 @@ BUILD_ID="${EAS_BUILD_ID}"
 BUILD_PLATFORM="${EAS_BUILD_PLATFORM}"
 OWNER='dtun'
 REPO='bible'
-TITLE='E2E Build Failure'
-BODY="The EAS build failed on platform ${BUILD_PLATFORM} with build ID ${BUILD_ID}. Please check the details."
+TITLE="${BUILD_PLATFORM}: E2E Build Failure"
+BODY="The EAS build failed on platform ${BUILD_PLATFORM} with build ID ${BUILD_ID}."
 
 # GitHub API URL
 URL="https://api.github.com/repos/$OWNER/$REPO/issues"
 
 # JSON data
 DATA=$(jq -n --arg title "$TITLE" --arg body "$BODY" \
-    '{title: $title, body: $body}')
+    '{"title": $title, "body": $body}')
 
 # Sending request to GitHub API to create issue
 curl -s -H "Authorization: token $TOKEN" \
