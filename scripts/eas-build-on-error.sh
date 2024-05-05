b3a965301cdded7bfd1e0d7edb6b922a1b856d58
dtun <dtun@me.com>
2024-05-05 11:21:42 -0700

    Rename app

--- scripts/eas-build-on-error.sh
+++ scripts/eas-build-on-error.sh
@@ -5,5 +5,5 @@ curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer ${GITHUB_API_KEY}" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/dtun/bible/issues \
+  https://api.github.com/repos/dtun/bible-memo/issues \
   -d "{\"title\":\"EAS Build Failure: ${EAS_BUILD_PLATFORM}\",\"body\":\"The EAS build failed for build ID ${EAS_BUILD_ID}.\",\"assignees\":[\"dtun\"]}"
