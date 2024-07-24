6043162991671704641108b0ddebd39b114702fe
dtun <dtun@me.com>
2024-07-23 19:10:22 -0700

    Update BibleTranslationListItem tests

--- components/__tests__/BibleTranslationListItem.test.tsx
+++ components/__tests__/BibleTranslationListItem.test.tsx
@@ -4,39 +4,42 @@ import { render, screen, userEvent } from "@/test-utils";
 import { bibleTranslationBuilder } from "@/builders/bibleTranslation";
 import { BibleTranslationListItem } from "../BibleTranslationListItem";
 
-let data = bibleTranslationBuilder();
+let bible = bibleTranslationBuilder();
 let onPress = jest.fn();
 
 it(`renders <BibleTranslationListItem />`, () => {
-  render(<BibleTranslationListItem item={{ ...data, onPress }} />);
+  render(<BibleTranslationListItem item={{ ...bible, onPress }} />);
 
-  expect(screen).toMatchSnapshot();
+  expect(screen.getByText(bible.abbreviationLocal)).toBeTruthy();
+  expect(screen.getByText(bible.nameLocal)).toBeTruthy();
 });
 
 it(`renders selected state`, () => {
   render(
-    <BibleTranslationListItem item={{ ...data, onPress, selected: true }} />
+    <BibleTranslationListItem item={{ ...bible, onPress, selected: true }} />
   );
 
-  expect(screen.getByLabelText(data.nameLocal)).toHaveProperty(
+  expect(screen.getByLabelText(bible.nameLocal)).toHaveProperty(
     "props.accessibilityState",
     { selected: true }
   );
+  expect(screen.getByText("✓")).toBeTruthy();
 
   screen.rerender(
-    <BibleTranslationListItem item={{ ...data, onPress, selected: false }} />
+    <BibleTranslationListItem item={{ ...bible, onPress, selected: false }} />
   );
 
-  expect(screen.getByLabelText(data.nameLocal)).toHaveProperty(
+  expect(screen.getByLabelText(bible.nameLocal)).toHaveProperty(
     "props.accessibilityState",
     { selected: false }
   );
+  expect(() => screen.getByText("✓")).toThrow();
 });
 
 it(`calls onPress with id`, async () => {
-  render(<BibleTranslationListItem item={{ ...data, onPress }} />);
+  render(<BibleTranslationListItem item={{ ...bible, onPress }} />);
 
-  await userEvent.press(screen.getByLabelText(data.nameLocal));
+  await userEvent.press(screen.getByLabelText(bible.nameLocal));
 
-  expect(onPress).toHaveBeenCalledWith(data.id);
+  expect(onPress).toHaveBeenCalledWith(bible.id);
 });
