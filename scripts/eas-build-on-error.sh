a3dc99d3399ef712e6c8c9238c13bc314a47f791
Danny Tunney <dtun@me.com>
2025-03-21 20:59:30 -0700

    Remove EAS build success and error scripts from project and update package.json accordingly

--- scripts/eas-build-on-error.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-curl -L \
-  -X POST \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer ${GITHUB_API_KEY}" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/dtun/bible-memo/issues \
-  -d "{\"title\":\"EAS Build Failure: ${EAS_BUILD_PLATFORM}\",\"body\":\"The EAS build failed for build ID ${EAS_BUILD_ID}.\",\"assignees\":[\"dtun\"]}"
